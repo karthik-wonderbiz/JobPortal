e0bf42a7b9e121fa9f87372bac139f51161e01e2
Yash-WB <yash.harad@wonderbiz.in>
2024-07-16 14:57:48 +0530

    updated base entity

---
+++ JobPortal.API/Migrations/20240716092501_UpdatedBaseTable.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobPortal.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedBaseTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_employmentTypes",
+                table: "employmentTypes");
+
+            migrationBuilder.RenameTable(
+                name: "employmentTypes",
+                newName: "EmploymentTypes");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_EmploymentTypes",
+                table: "EmploymentTypes",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_EmploymentTypes",
+                table: "EmploymentTypes");
+
+            migrationBuilder.RenameTable(
+                name: "EmploymentTypes",
+                newName: "employmentTypes");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_employmentTypes",
+                table: "employmentTypes",
+                column: "Id");
+        }
+    }
+}
