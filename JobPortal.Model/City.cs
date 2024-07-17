e73f52fa436b6d1943292e084c3116bbaa2a4714
karthik-wonderbiz <karthik.balakrishnan@wonderbiz.in>
2024-07-17 13:01:35 +0530

    Add City Model

---
+++ JobPortal.Model/City.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobPortal.Model
+{
+    [Table("Cities")]
+    [Index(nameof(CityName), IsUnique = true)]
+    public class City : BaseEntity
+    {
+        [Required(ErrorMessage = "City Name is Required!")]
+        [StringLength(100)]
+        public string CityName { get; set; } = string.Empty;
+        public string CityCode { get; set; } = string.Empty;
+        public bool IsActive { get; set; } = true;
+    }
+}
