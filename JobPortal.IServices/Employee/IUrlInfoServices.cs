d0e39534d7194ca740a57b31737e43645a5bcdf0
Anirudh Gupta <anirudh.gupta@wonderbiz.in>
2024-07-19 17:34:53 +0530

    Added UrlInfo

---
+++ JobPortal.IServices/Employee/IUrlInfoServices.cs
@@ -0,0 +1,25 @@
+﻿using JobPortal.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static JobPortal.DTO.Employee.UrlInfoDto;
+
+namespace JobPortal.IServices.Employee
+{
+    public interface IUrlInfoServices
+    {
+        Task<IEnumerable<GetUrlInfoDto>> GetUrlInfosAsync();
+
+        Task<GetUrlInfoDto> GetUrlInfoAsync(long id);
+
+        Task<GetUrlInfoDto> CreateUrlInfoAsync(CreateUrlInfoDto createUrlInfoDto);
+
+        Task<GetUrlInfoDto> UpdateUrlInfoAsync(long id, UpdateUrlInfoDto updateUrlInfoDto);
+
+        Task<bool> DeleteUrlInfoAsync(long id);
+
+        Task<IEnumerable<GetUrlInfoDto>> GetUrlInfoByUserId(long userId);
+    }
+}
---
+++ JobPortal.Repository/Employee/UrlInfoRepository.cs
@@ -0,0 +1,76 @@
+﻿using JobPortal.Data;
+using JobPortal.IRepository;
+using JobPortal.IRepository.Employee;
+using JobPortal.Model.Employee;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static JobPortal.DTO.Employee.UrlInfoDto;
+
+namespace JobPortal.Repository.Employee
+{
+    public class UrlInfoRepository : Repository<UrlInfo>, IUrlInfoRepository
+    {
+        private readonly JobPortalDbContext _dbContext;
+
+        public UrlInfoRepository(JobPortalDbContext dbcontext) : base(dbcontext)
+        {
+            _dbContext = dbcontext;
+        }
+
+        public async Task<IEnumerable<UrlInfo>> GetByUserId(long userId)
+        {
+            try
+            {
+                var urlInfos = await _dbContext.urlInfos
+                    .Include(li => li.User)
+                    .Include(li => li.UrlName)
+                    .Where(li => li.UserId == userId)
+                    .ToListAsync();
+
+                return urlInfos;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<IEnumerable<UrlInfo>> GetAllAsync()
+        {
+            try
+            {
+                var urlInfos = await _dbContext.urlInfos
+                    .Include(li => li.User)
+                    .Include(li => li.UrlName)
+                    .ToListAsync();
+
+                return urlInfos;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<UrlInfo> GetAsync(long id)
+        {
+            try
+            {
+                var urlInfo = await _dbContext.urlInfos
+                    .Include(li => li.User)
+                    .Include(li => li.UrlName)
+                    .FirstOrDefaultAsync(li => li.Id == id); ;
+
+                return urlInfo;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
