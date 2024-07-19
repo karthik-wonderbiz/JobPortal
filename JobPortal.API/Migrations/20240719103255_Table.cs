a22a65491a90d23acbc36c526fbbd5d0604b8496
Yash-WB <yash.harad@wonderbiz.in>
2024-07-19 16:05:38 +0530

    Migrate Experience

---
+++ JobPortal.API/Migrations/20240719103255_Table.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobPortal.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "certificationInfos",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<long>(type: "bigint", nullable: false),
+                    CertficateName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OrganisationName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IssueDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ExpiryDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    SkillAcquired = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CertificateURL = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UpdatedBy = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_certificationInfos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_certificationInfos_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "urlInfos",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<long>(type: "bigint", nullable: false),
+                    UrlNameId = table.Column<long>(type: "bigint", nullable: false),
+                    UrlValue = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UpdatedBy = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_urlInfos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_urlInfos_UrlNames_UrlNameId",
+                        column: x => x.UrlNameId,
+                        principalTable: "UrlNames",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_urlInfos_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WorkExperienceInfos",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<long>(type: "bigint", nullable: false),
+                    WorkTypeId = table.Column<long>(type: "bigint", nullable: false),
+                    EmploymentTypeId = table.Column<long>(type: "bigint", nullable: false),
+                    DesignationId = table.Column<long>(type: "bigint", nullable: false),
+                    CompanyName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    SkillsUsed = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UpdatedBy = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WorkExperienceInfos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_WorkExperienceInfos_Designations_DesignationId",
+                        column: x => x.DesignationId,
+                        principalTable: "Designations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_WorkExperienceInfos_EmploymentTypes_EmploymentTypeId",
+                        column: x => x.EmploymentTypeId,
+                        principalTable: "EmploymentTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_WorkExperienceInfos_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_WorkExperienceInfos_WorkTypes_WorkTypeId",
+                        column: x => x.WorkTypeId,
+                        principalTable: "WorkTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_certificationInfos_UserId",
+                table: "certificationInfos",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_urlInfos_UrlNameId",
+                table: "urlInfos",
+                column: "UrlNameId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_urlInfos_UserId",
+                table: "urlInfos",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WorkExperienceInfos_DesignationId",
+                table: "WorkExperienceInfos",
+                column: "DesignationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WorkExperienceInfos_EmploymentTypeId",
+                table: "WorkExperienceInfos",
+                column: "EmploymentTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WorkExperienceInfos_UserId",
+                table: "WorkExperienceInfos",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WorkExperienceInfos_WorkTypeId",
+                table: "WorkExperienceInfos",
+                column: "WorkTypeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "certificationInfos");
+
+            migrationBuilder.DropTable(
+                name: "urlInfos");
+
+            migrationBuilder.DropTable(
+                name: "WorkExperienceInfos");
+        }
+    }
+}
