dd9dd165ff32a688d3275f5fef921dc47e6431a9
Yash-WB <yash.harad@wonderbiz.in>
2024-07-23 09:57:03 +0530

    updated base entity

--- JobPortal.Model/BaseEntity.cs
+++ JobPortal.Model/BaseEntity.cs
@@ -16,7 +16,7 @@ namespace JobPortal.Model
         public long Id { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
-        public string CreatedBy { get; set; } = string.Empty;
-        public string UpdatedBy { get; set; } = string.Empty;
+        public long CreatedBy { get; set; }
+        public long UpdatedBy { get; set; }
     }
 }
